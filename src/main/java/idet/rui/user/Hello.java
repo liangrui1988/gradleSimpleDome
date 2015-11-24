fc7f800e7bc7d7f0bce8dbb47bc6517ba80578a6
liangrui <liangrui@20150116-4876.KUGOU.GZ>
2015-11-24 15:56:38 +0800

    init

---
+++ src/main/java/idet/rui/user/Hello.java
@@ -0,0 +1,11 @@
+package idet.rui.user;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		
+	}
+
+}
---
+++ src/test/java/idet/HelloTest.java
@@ -0,0 +1,12 @@
+package idet;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		System.out.println("hello test......");
+
+	}
+}
